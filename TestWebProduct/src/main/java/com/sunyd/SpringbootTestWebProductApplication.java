f71f8bb91876d1c143b9695bb3d2fb90f397a9b8
sunyindong.syd <sunyindong.syd@alimac.local>
2022-07-19 19:33:48 +0800

    统一返回状态码和异常码

---
+++ TestWebProduct/src/main/java/com/sunyd/SpringbootTestWebProductApplication.java
@@ -0,0 +1,18 @@
+package com.sunyd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+
+@EnableAsync //开启异步注解功能
+@EnableScheduling//开启异步定时功能的注解
+@SpringBootApplication
+public class SpringbootTestWebProductApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringbootTestWebProductApplication.class, args);
+  }
+
+}
